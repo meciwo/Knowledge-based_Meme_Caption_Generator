ad3d62d50828609da4d7e8de64c57c695304346d
meciwo <shanshan0474@gmail.com>
2021-04-09 03:10:08 +0900

    build knowledge graph emebedding

---
+++ src/build_KGEmbed.py
@@ -0,0 +1,24 @@
+from wikipedia2vec import Wikipedia2Vec
+import numpy as np
+
+model_file = "data/enwiki_20180420_win10_100d.pkl.bz2"
+wiki2vec = Wikipedia2Vec.load(model_file)
+
+COCO_INSTANCE_CATEGORY_NAMES = [
+    '__background__', 'person', 'bicycle', 'car', 'motorcycle', 'airplane', 'bus',
+    'train', 'truck', 'boat', 'traffic light', 'fire hydrant', 'N/A', 'stop sign',
+    'parking meter', 'bench', 'bird', 'cat', 'dog', 'horse', 'sheep', 'cow',
+    'elephant', 'bear', 'zebra', 'giraffe', 'N/A', 'backpack', 'umbrella', 'N/A', 'N/A',
+    'handbag', 'tie', 'suitcase', 'frisbee', 'skis', 'snowboard', 'sports ball',
+    'kite', 'baseball bat', 'baseball glove', 'skateboard', 'surfboard', 'tennis racket',
+    'bottle', 'N/A', 'wine glass', 'cup', 'fork', 'knife', 'spoon', 'bowl',
+    'banana', 'apple', 'sandwich', 'orange', 'broccoli', 'carrot', 'hot dog', 'pizza',
+    'donut', 'cake', 'chair', 'couch', 'potted plant', 'bed', 'N/A', 'dining table',
+    'N/A', 'N/A', 'toilet', 'N/A', 'tv', 'laptop', 'mouse', 'remote', 'keyboard', 'cell phone',
+    'microwave', 'oven', 'toaster', 'sink', 'refrigerator', 'N/A', 'book',
+    'clock', 'vase', 'scissors', 'teddy bear', 'hair drier', 'toothbrush'
+]
+
+dic = np.array([wiki2vec.get_word_vector(obj).tolist()
+                for obj in COCO_INSTANCE_CATEGORY_NAMES])
+np.save("data/KGEmbed.npy")
